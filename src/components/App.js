0a4a74eb9b67cc15c568f0fbb8034581699080bc
Alexandra Weaver <aweaver0124@gmail.com>
2021-12-21 20:27:42 -0600

    commit tree d3acd699ea4cc6ce4734441e879278250a3ae253

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useState, useEffect } from 'react';
+
+function App () {
+    const [showDog, setShowDog] = useState("");
+    const [isLoaded, setIsLoaded] = useState(false);
+
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => response.json())
+        .then((data) => {
+                setShowDog(data.message)
+                setIsLoaded(true);
+            });
+    }, [])
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+    return <img src={showDog} alt="A Random Dog" />
+        
+    
+}
+
+export default App
